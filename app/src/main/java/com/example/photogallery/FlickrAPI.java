bf9bf40961cd16f96c846eb28447037494b7ecca
nadiabenko <nadiabenkoo@gmail.com>
2020-12-17 23:48:05 +0700

    Add FlickrAPI, ServiceAPI

---
+++ app/src/main/java/com/example/photogallery/FlickrAPI.java
@@ -0,0 +1,14 @@
+package com.example.photogallery;
+
+import android.telecom.Call;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface FlickrAPI {
+    @GET("services/rest/?method=flickr.photos.getRecent&api_key=30f73b5de236fe4fcd78f3b6c91e8ad2&extras=url_s&per_page=2&page=2&format=json&nojsoncallback=1")
+            Call<PhotosResponse> getRecent();
+            @GET("services/rest/?method=flickr.photos.search&api_key=30f73b5de236fe4fcd78f3b6c91e8ad2&extras=url_s&per_page=2&page=2&format=json&nojsoncallback=1")
+            Call<PhotosResponse> getSearchPhotos(@Query("text") String keyWord);
+
+}
